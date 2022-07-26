0c874ddc699f9de970e1c02508e0af12b9888abf
강선구 <seongu97@naver.com>
2022-07-26 21:13:59 +0900

    Docs(nav): 엘리먼트 추가

--- JS/Container/container.js
+++ JS/Container/container.js
@@ -1,11 +1,13 @@
 'use strict';
 
 import Header from "../Header/header.js";
+import Nav from "../Nav/nav.js";
 
 export default class Container {
     constructor(Create, index) {
         this.container = new Create('container', 'section', ['class'], ['container'], '', 1, index, false, '', '');
     
         this.header = new Header(Create, this.container.name);
+        this.nav = new Nav(Create, this.container.name);
     }
 }
---
+++ JS/Nav/nav.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export default class Nav {
+    constructor(Create, container) {
+        this.nav = new Create('nav', 'nav', [''], [''], '', 1, container, false, '', '');
+    }
+}
