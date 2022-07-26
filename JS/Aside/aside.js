a41bbb30cb0c8770e7e99d391e7ae7cb05a6c475
강선구 <seongu97@naver.com>
2022-07-26 21:34:07 +0900

    Docs(aside): 엘리먼트 추가

---
+++ JS/Aside/aside.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export default class Aside {
+    constructor(Create, container) {
+        this.aside = new Create('aside', 'aside', [''], [''], '', 1, container, false, '', '');
+    }
+}
--- JS/Container/container.js
+++ JS/Container/container.js
@@ -1,11 +1,13 @@
 'use strict';
 
 import Main from "../Main/main.js";
+import Aside from "../Aside/aside.js";
 
 export default class Container {
     constructor(Create, index) {
         this.container = new Create('container', 'section', ['class'], ['container'], '', 1, index, false, '', '');
         
         this.main = new Main(Create, this.container.name);
+        this.aside = new Aside(Create, this.container.name);
     }
 }
