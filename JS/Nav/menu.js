f80a6fdf2630346a25d2433a33896b2b72dd1d8f
강선구 <seongu97@naver.com>
2022-07-28 23:53:03 +0900

    Feat(.menu): 클릭 효과 제거

--- JS/Nav/menu.js
+++ JS/Nav/menu.js
@@ -6,24 +6,6 @@ export default class Menu {
             
             for(let i = 0; i < this.menus.length; i++) {
                 this.menus[i].classList.remove('active');
-                
-                const {x, y, width, height} = this.menus[i].getBoundingClientRect();
-                
-                const radius = Math.sqrt(width * height + width * height);
-
-                this.menus[i].style.setProperty('--diameter', radius * 2 + 'px');
-
-                const {clientX, clientY} = e;
-                const left = (clientX - x - radius) / width * 100 + '%';
-                const top = (clientY - y - radius) / height * 100 + '%';
-
-                this.menus[i].style.setProperty('--left', left);
-                this.menus[i].style.setProperty('--top', top);
-                this.menus[i].style.setProperty('--a', '');
-                
-                setTimeout(() => {
-                    this.menus[i].style.setProperty('--a', 'effect 250ms linear');
-                })
             }
 
             this.target = e.target;
