8732bea55f03f56b3a145bde760681a0728f3f52
강선구 <seongu97@naver.com>
2022-09-02 19:56:43 +0900

    Refactor(picture): 이미지 안나오는 문제 수정

--- Javascript/Box/picture.js
+++ Javascript/Box/picture.js
@@ -2,6 +2,6 @@
 
 export default class Picture {
     constructor(creator, profile) {
-        const picture = new creator('picture', 'img', ['class', 'src'], ['picture profile-bd-w', '/Img/취업사진1.jpg'], '', 1, profile, false, '', '');
+        const picture = new creator('picture', 'img', ['class', 'src', 'alt'], ['picture profile-bd-w', 'Img/취업사진1.jpg', '취업사진1.jpg'], '', 1, profile, false, '', '');
     }
 }
