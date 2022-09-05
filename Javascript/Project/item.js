1dde8132c3eaa13d7db572f6d9a3df3a1c64e568
강선구 <seongu97@naver.com>
2022-09-05 23:22:58 +0900

    Docs(item-img): 엘리먼트 제거

--- Javascript/Project/item.js
+++ Javascript/Project/item.js
@@ -3,10 +3,5 @@
 export default class Item {
     constructor(creator, project) {
         const item = new creator('item', 'div', 'class', 'item project-w', '', 6, project, false, '', '');
-
-
-        project.childNodes.forEach(element => {
-            const item_img = new creator('item_img', 'div', 'class', 'item-img', '', 1, element, false, '', ''); 
-        });   
     }
 }
